3e18c03d23e14ac2f3a05942420924b5c7c74115
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-05 15:32:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+  collection =[]
+  i =0 
+  while i < array.length 
+    collection << yield(array[i])
+    i+=1 
+  end
+  for element in array
+  collection << yield element
+  end
+  collection
+end
